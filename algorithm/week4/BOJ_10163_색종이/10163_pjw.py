bab897ffcfb7229af29fcb6ee2952a8c6ba0e754
박지원 <tlsxp13@gmail.com>
2024-02-28 22:01:47 +0900

    지원

--- algorithm/week4/BOJ_10163_색종이/10163_pjw.py
+++ algorithm/week4/BOJ_10163_색종이/10163_pjw.py
@@ -1,10 +1,10 @@
 N = int(input())
 arr = [[0]*1001 for _ in range(1001)]
 col_pap = [list(map(int, input().split())) for _ in range(N)]  # 색종이에관한 입력
-
+empty = []
 for i in range(0, N):
     alpha = 0
-    result = 0
+    result = 0    
     for j in range(i+1, N):
         max1 = max(col_pap[i][0], col_pap[j][0])
         max2 = max(col_pap[i][1], col_pap[j][1])
@@ -13,9 +13,10 @@ for i in range(0, N):
         if min2-max2+1 > 0 and min1-max1 + 1 > 0:
             alpha = col_pap[i][2]*col_pap[i][3] - (min2 - max2 +1) * (min1 - max1 +1)
             result += alpha
-    if result >= 100:
-        print(result - 100)
+    if result >= col_pap[0][2]*col_pap[0][3]:
+        empty.append(result - col_pap[0][2]*col_pap[0][3])
     else:
-        print(result)
+        empty.append(result)
 
+print(*empty)
 print(col_pap[-1][2]*col_pap[-1][3])
